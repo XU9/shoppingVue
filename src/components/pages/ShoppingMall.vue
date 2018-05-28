c4a537da96bc56b027d90783f33cec98da60b93a
simple_XU <nhaxyz@163.com>
2018-05-29 00:08:08 +0800

    shoppingMall

---
+++ src/components/pages/ShoppingMall.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <van-row class="test-row">
+          <van-col span="4">span:4</van-col>
+          <van-col span="16">span:16</van-col>
+          <van-col span="4">span:4</van-col>
+        </van-row>
+{{msg}}
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "shopping Mall"
+    };
+  }
+};
+</script>
+
+<style scoped>
+.test-row{
+  text-align: center;
+}
+</style>
